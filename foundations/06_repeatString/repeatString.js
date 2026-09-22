06f8c6cb52d28be89bf98aa6736efed5d2198998
rin.joshua@gmail.com <rin.joshua@gmail.com>
2026-09-22 10:38:33 -0500

    Excercise 06 complete and passes all test cases

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let repeatString = '';
+    if(num < 0){
+        repeatString = 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        
+        if(str === "" || num === 0){
+            repeatString = str;
+            break;
+        }
+        else {
+        repeatString += str;
+        };
+        
 };
+return repeatString;
+}
+console.log("hello world");
 
 // Do not edit below this line
 module.exports = repeatString;
