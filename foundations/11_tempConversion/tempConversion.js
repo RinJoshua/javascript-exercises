dcd16703e7e0c90d5f7dcd31dd08e5ba57149eca
rin.joshua@gmail.com <rin.joshua@gmail.com>
2026-09-23 12:44:55 -0500

    Excercise 11 completed with all teest cases passing

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * 5/9;
+  if(Number.isInteger(cTemp)){
+    return cTemp;}
+    
+  return cTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = (cTemp * 1.8) +32;
+  if(Number.isInteger(fTemp)){
+    return fTemp;
+  }
+  return fTemp.toFixed(1);
 };
 
 // Do not edit below this line
